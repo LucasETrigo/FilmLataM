ddceef5504b7d474d520951fa0fcc0a28ab22fe5
Lucas Trigo <lucasetrigo@gmail.com>
2022-08-26 17:09:37 -0300

    Adding some changes v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
+
+import useStyles from './styles';
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
 
 function App() {
-    return <div>App</div>;
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <NavBar />
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <BrowserRouter>
+                    <Routes>
+                        <Route
+                            path='/movie/:id'
+                            element={<MovieInformation />}
+                        />
+                        <Route path='/actors/:id' element={<Actors />} />
+                        <Route path='/' element={<Movies />} />
+                        <Route path='/profile/:id' element={<Profile />} />
+                    </Routes>
+                </BrowserRouter>
+            </main>
+        </div>
+    );
 }
 
 export default App;
